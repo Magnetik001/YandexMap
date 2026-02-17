4ba705013986dbd6dbe7afaaa34efd318b60fdb3
max <shaman_kvazar@mail.ru>
2026-02-17 20:10:45 +0300

    1

--- main.py
+++ main.py
@@ -1,60 +1,17 @@
-import os
-import sys
-import requests
-import arcade
+# This is a sample Python script.
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-WINDOW_TITLE = "MAP"
-MAP_FILE = "map.png"
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-class GameView(arcade.Window):
-    def setup(self):
-        self.get_image()
-
-    def on_draw(self):
-        self.clear()
-
-        arcade.draw_texture_rect(
-            self.background,
-            arcade.LBWH(
-                (self.width - self.background.width) // 2,
-                (self.height - self.background.height) // 2,
-                self.background.width,
-                self.background.height
-            ),
-        )
-
-    def get_image(self):
-        server_address = 'https://static-maps.yandex.ru/v1?'
-        api_key = '2ac33b20-348f-4429-86d1-1ab126c72677'
-        params = {
-            "apikey": api_key,
-            "ll": "0,0",
-            "spn": "180,90",
-            "size": "500,450"
-        }
-
-        response = requests.get(server_address, params=params)
-
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-
-        with open(MAP_FILE, "wb") as file:
-            file.write(response.content)
-
-        self.background = arcade.load_texture(MAP_FILE)
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def main():
-    gameview = GameView(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-    gameview.setup()
-    arcade.run()
-    os.remove(MAP_FILE)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-if __name__ == "__main__":
-    main()
