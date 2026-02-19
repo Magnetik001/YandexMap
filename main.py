fdfdde03d6d7b50c33b833de28528191f987f7f3
Егор <@>
2026-02-20 00:37:38 +0300

    Задача 11

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import os
 import sys
 import requests
@@ -6,8 +7,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QInputDialog, QVB
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 
-WINDOW_WIDTH = 450
-WINDOW_HEIGHT = 350
+WINDOW_WIDTH = 550
+WINDOW_HEIGHT = 550
 WINDOW_TITLE = "MAP"
 MAP_FILE = "map.png"
 
@@ -34,7 +35,6 @@ class YandexMap(QMainWindow):
         self.find_line.textChanged.connect(self.find)
         self.layout.addWidget(self.find_line)
 
-
         self.info_label = QLabel()
         self.layout.addWidget(self.info_label)
 
@@ -47,12 +47,16 @@ class YandexMap(QMainWindow):
         self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
         self.layout.addWidget(self.image_label)
 
-        self.get_coordinates(address)
-        self.zoom = 0.002
+        self.zoom = 17
+
         self.theme = "light"
         self.postal_code = False
-        self.get_response()
+        self.block_finder = True
 
+        self.get_coordinates(address)
+        self.find_line.blockSignals(self.block_finder)
+
+        self.get_response()
         self.image()
 
     def image(self):
@@ -62,39 +66,47 @@ class YandexMap(QMainWindow):
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key.Key_PageUp:
-            self.zoom /= 2 if self.zoom > 0.0005 else 1
-            self.get_response()
-            self.image()
-        if e.key() == Qt.Key.Key_PageDown:
-            self.zoom *= 2 if self.zoom < 1 else 1
-            self.get_response()
-            self.image()
+            if self.zoom < 21:
+                self.zoom += 1
+                self.get_response()
+                self.image()
+        elif e.key() == Qt.Key.Key_PageDown:
+            if self.zoom > 0:
+                self.zoom -= 1
+                self.get_response()
+                self.image()
 
-        step = self.zoom * 0.5
+        pix_w = 450
+        lon_span = 360 * pix_w / (256 * 2 ** self.zoom)
+        lat_span = lon_span * math.cos(math.radians(float(self.coordinates[1])))
+
+        step_lat = lat_span * 0.5
+        step_lon = lon_span * 0.5
+
         lon = float(self.coordinates[0])
         lat = float(self.coordinates[1])
 
         if e.key() == Qt.Key.Key_W:
-            if lat < 90:
-                lat += step
+            if lat < 85:
+                lat += step_lat
                 self.coordinates = [str(lon), str(lat)]
                 self.get_response()
                 self.image()
         elif e.key() == Qt.Key.Key_S:
-            if lat > -90:
-                lat -= step
+            if lat > -85:
+                lat -= step_lat
                 self.coordinates = [str(lon), str(lat)]
                 self.get_response()
                 self.image()
         elif e.key() == Qt.Key.Key_A:
             if lon > -180:
-                lon -= step
+                lon -= step_lon
                 self.coordinates = [str(lon), str(lat)]
                 self.get_response()
                 self.image()
         elif e.key() == Qt.Key.Key_D:
             if lon < 180:
-                lon += step
+                lon += step_lon
                 self.coordinates = [str(lon), str(lat)]
                 self.get_response()
                 self.image()
@@ -111,26 +123,75 @@ class YandexMap(QMainWindow):
                 self.info_label.setText(f"{self.info2} {self.info1}")
             else:
                 self.info_label.setText(f"{self.info1}")
+        elif e.key() == Qt.Key.Key_Z:
+            self.block_finder = not self.block_finder
+            self.find_line.blockSignals(self.block_finder)
+        elif e.key() == Qt.Key.Key_Return:
+            self.find()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            pixmap = self.image_label.pixmap()
+            if not pixmap:
+                return
+
+            pos = self.image_label.mapFromGlobal(event.globalPosition().toPoint())
+
+            pix_w = pixmap.width()
+            pix_h = pixmap.height()
+            label_w = self.image_label.width()
+            label_h = self.image_label.height()
+
+            scale = min(label_w / pix_w, label_h / pix_h)
+            real_w = pix_w * scale
+            real_h = pix_h * scale
+            offset_x = (label_w - real_w) / 2
+            offset_y = (label_h - real_h) / 2
+
+            real_x = (pos.x() - offset_x) / scale
+            real_y = (pos.y() - offset_y) / scale
+
+            if 0 <= real_x <= pix_w and 0 <= real_y <= pix_h:
+                center_lon = float(self.coordinates[0])
+                center_lat = float(self.coordinates[1])
+
+                lon_span = 360 * pix_w / (256 * 2 ** self.zoom)
+
+                lat_span = lon_span * math.cos(math.radians(center_lat))
+
+                x_ratio = real_x / pix_w
+                y_ratio = real_y / pix_h
+
+                new_lon = center_lon + (x_ratio - 0.5) * lon_span
+                new_lat = center_lat - (y_ratio - 0.5) * lat_span
+
+                click_address = f"{new_lon},{new_lat}"
+                self.get_coordinates(click_address, 2)
+                self.get_response()
+                self.image()
 
     def get_response(self):
-        server_address_static_map = 'https://static-maps.yandex.ru/v1?  '
+        server_address_static_map = 'https://static-maps.yandex.ru/v1?'
         api_key_static_map = '2ac33b20-348f-4429-86d1-1ab126c72677'
 
         params_static_map = {
             "apikey": api_key_static_map,
             "ll": f"{self.coordinates[0]},{self.coordinates[1]}",
-            "spn": f"{self.zoom},{self.zoom}",
-            "size": "400,300",
+            "z": str(self.zoom),
+            "size": "450,450",
             "theme": self.theme,
             "pt": self.pt
         }
 
         response = requests.request("GET", server_address_static_map, params=params_static_map)
 
-        with open(MAP_FILE, "wb") as file:
-            file.write(response.content)
+        if response.status_code == 200:
+            with open(MAP_FILE, "wb") as file:
+                file.write(response.content)
+        else:
+            print("Ошибка получения карты:", response.status_code)
 
-    def get_coordinates(self, address):
+    def get_coordinates(self, address, mode=1):
         params_geocode = {
             "apikey": api_key_geocode,
             "geocode": address,
@@ -138,18 +199,21 @@ class YandexMap(QMainWindow):
         }
 
         response1 = requests.request("GET", server_address_geocode, params=params_geocode)
-        print(response1.url)
         response = response1.json()
 
         if response1.status_code == 200 and len(response["response"]["GeoObjectCollection"]["featureMember"]) > 0:
-            self.coordinates = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"][
-                "pos"].split()
+            if mode == 1:
+                self.coordinates = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"][
+                    "pos"].split()
             self.info1 = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["name"]
             try:
                 self.info2 = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
             except KeyError:
                 self.info2 = ""
-            self.pt = f"{self.coordinates[0]},{self.coordinates[1]},pm2dgl"
+            if mode == 1:
+                self.pt = f"{self.coordinates[0]},{self.coordinates[1]},pm2dgl"
+            elif mode == 2:
+                self.pt = f"{address},pm2dgl"
         else:
             self.coordinates = ["0", "0"]
             self.info1 = "Ошибка адреса"
